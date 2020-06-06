00adf042819b4de2b33b2dbb8d83d556f1f55753
Kanami <kanami.h217@gmail.com>
2020-06-05 21:12:57 -0400

    modified Body

--- src/InstaCard/Body/index.js
+++ src/InstaCard/Body/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BodyMain from './Main';
-import MainImg from '../../images/image-one.jpg';
+import imageOne from '../../images/image-one.jpg';
+import imageTwo from '../../images/image-two.jpg';
 
-const Body = ({ theme }) => (
+const Body = ({ theme, chosenId, data }) => {
+  console.log(chosenId)
+  return(
+  // ↑ chosenId と dataが抜けてます。const Body = ({ props }) => {return(JSX);}; の形式になっているかも見直しましょう
+  // 画像データも、今は上記の画像１を手動で読み込ませていますので、「その時取得された画像データ」を当てはめてあげる必要があります
+  // returnでJSXを書く前の範囲で、console.log(chosenId); と検証してみてください。選択されたカードのidがコンソールに出ると思います
   <section className="card-body">
     <div className="card-image">
-      <img alt="メイン画像" src={MainImg} />
+      <img alt="メイン画像" src={imageOne || imageTwo} />
     </div>
-    <BodyMain theme={theme} />
+    <BodyMain 
+      theme={theme}
+      chosenId={chosenId}
+      data={data}
+      />
   </section>
-);
+)}
 
 Comment.propTypes = {
   theme: PropTypes.string.isRequired
