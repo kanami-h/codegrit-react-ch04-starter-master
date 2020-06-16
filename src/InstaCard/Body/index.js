2e884f96f079ec7c570fca4607a1f6eb9e438648
Kanami <kanami.h217@gmail.com>
2020-06-15 22:53:52 -0400

    modified imageUrl

--- src/InstaCard/Body/index.js
+++ src/InstaCard/Body/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BodyMain from './Main';
-import imageOne from '../../images/image-one.jpg';
-import imageTwo from '../../images/image-two.jpg';
+import cardDataOne from '../../CardDataUtils';
+import cardDataTwo from '../../CardDataUtils';
 
 const Body = ({ theme, chosenId, data }) => {
   console.log(chosenId)
+  const { imageUrl } = data;
   return(
   // ↑ chosenId と dataが抜けてます。const Body = ({ props }) => {return(JSX);}; の形式になっているかも見直しましょう
   // 画像データも、今は上記の画像１を手動で読み込ませていますので、「その時取得された画像データ」を当てはめてあげる必要があります
   // returnでJSXを書く前の範囲で、console.log(chosenId); と検証してみてください。選択されたカードのidがコンソールに出ると思います
   <section className="card-body">
     <div className="card-image">
-      <img alt="メイン画像" src={imageOne || imageTwo} />
+      <img alt="メイン画像" src={data} />
     </div>
     <BodyMain 
       theme={theme}
@@ -24,12 +25,18 @@ const Body = ({ theme, chosenId, data }) => {
 
 Comment.propTypes = {// ここも以下のdefaultPropsに応じて追加が必要そうですね
   theme: PropTypes.string.isRequired,
-  chosenId: PropTypes.number.isRequired
+  chosenId: PropTypes.number.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  data: PropTypes.isRequired,
+  loadedOnce: PropTypes.array.isRequired
 }
 
 Comment.defaultProps = {// デフォルトの画像指定もしておいた方が良さそうですね
   theme: "light",
-  chosenId: 1
+  chosenId: 1,
+  isLoading: true,
+  data: null,
+  loadedOnce: false
 }
 
 export default Body;
