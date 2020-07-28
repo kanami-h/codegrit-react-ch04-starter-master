42fde4b38e3e7838d23ff72db15d78911edce6e5
Kanami <kanami.h217@gmail.com>
2020-07-28 11:29:18 -0400

    add propTypes, defaultProps

--- src/InstaCard/Body/Comments.js
+++ src/InstaCard/Body/Comments.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Comment from './Comment';
 
-const Comments = ({ data }) => (
-  <ul className="comments">
-    <Comment
-      username={
-        id === 1 ? cardDataOne : cardDataTwo
-      }
-      comment={} />
-    <Comment
-      username="testuser2"
-      comment="So cool!" />
-  </ul>
-);
+const Comments = ({ data }) => {
+  const { commentOne, commentTwo } = data
+  return (
+    <ul className="comments">
+      <Comment
+        username={commentOne.poster}
+        comment={commentOne.body} />
+      <Comment
+        username={commentTwo.poster}
+        comment={commentTwo.body} />
+    </ul>
+  );
+}
 
+Comments.propTypes = {
+  data: {
+    commentOne: {
+      poster: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired
+    },
+    commentTwo: {
+      poster: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired
+    }
+  } 
+}
+
+Comments.defaultProps = {
+  data: {
+    commentOne: {
+      poster: "commenter1",
+      body: "This is the insta-card"
+    },
+    commentTwo: {
+      poster: "commenter2",
+      body: "So cool!"
+    }
+  } 
+}
 export default Comments;
